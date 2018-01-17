39d86610e6bcc725fcb703d025612664757a6833
web_liuyaowei <web_liuyaowei@sina.com>
2018-01-17 18:06:53 +0800

    元宵点灯

--- cli-pc/src/components/lanternFestival/api/api.js
+++ cli-pc/src/components/lanternFestival/api/api.js
@@ -1,6 +1,8 @@
 import axios from './../../../http' 
 let base = window.BASEDOMAIN + '/app/lantern'
 
+// let base = 'http://192.168.3.68:8080'
+
 /*连接*/
 export const getShortUrl = params => { return axios.get(`${window.BASEDOMAIN}/app/link/getShorUrl?url=${params}`).then(res => res.data) }
 export const getMobileUrl   = params => { return axios.post(`${base}/getMobileUrl`, params).then(res => res.data) }
@@ -25,3 +27,52 @@ export const givePrize        = params => { return axios.post(`${base}/editNewYe
 /*新增，编辑*/
 export const saveAct          = params => { return axios.post(`${base}/addLantern`, params).then(res => res.data) }
 export const getActData       = params => { return axios.get(`${base}/getNewYear?id=${params}`).then(res => res.data) }
+
+function post(url, params) {
+    return axios.post(url, params)
+      .then(res => {
+          return res.data
+      })
+      .catch(error => {
+        return {code: 400}
+      })
+  }
+  
+  function get(url, params) {
+    return axios.get(url, params)
+      .then(res => {
+          return res.data
+      })
+      .catch(error => {
+        return {code: 400}
+      })
+  }
+
+export default {
+       // 分页获取元宵点灯活动列表 
+       getLanternList(params) {
+           return post(`${base}/getLanternList`, params)
+       },
+       // 删除元宵点灯活动 
+       delLantern(params) {
+           return post(`${base}/delLantern`, params) 
+       }, 
+       // 获取手机端链接 
+       getMobileUrl(params) {
+           return post(`${base}/getMobileUrl`, params)   
+       },
+       // 获取奖品类型列表 
+       getLanternPrizeType(params) {
+           return post(`${base}/getLanternPrizeType`, params)    
+       },
+       // 新增元宵点灯活动 
+       addLantern(params) {
+           return post(`${base}/addLantern`, params)    
+       },
+       // 通过活动id查询元宵点灯活动 
+       getActivityById(params) {
+        return post(`${base}/getActivityById`, params) 
+       }
+}
+
+
