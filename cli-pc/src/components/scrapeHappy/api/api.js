213a0edb338049c0f04940b5e38a92fd6963f83b
yanhuiting <1227406194@qq.com>
2018-01-23 09:14:47 +0800

    1=23/1

--- cli-pc/src/components/scrapeHappy/api/api.js
+++ cli-pc/src/components/scrapeHappy/api/api.js
@@ -1,4 +1,18 @@
 import axios from './../../../http' 
-let base = window.BASEDOMAIN
-/*短连接*/
-export const requestgetShortUrl = params => { return axios.get(`${base}/app/link/getShorUrl?url=${params}`).then(res => res.data) }
+let base = window.BASEDOMAIN + '/app/scratch'
+
+//新增刮刮乐活动/
+export const saveAct = params => { return axios.post(`${base}/addScratch`, params).then(res => res.data) }
+
+//统计刮刮乐活动总数/
+export const getActCount = params => { return axios.post(`${base}/countScratch`, params).then(res => res.data) }
+
+//通过活动id查询刮刮乐活动/
+export const getActivityById= params => { return axios.post(`${base}/getActivityById`, params).then(res => res.data) }
+
+/*获取手机端链接*/
+export const getShortUrl = params => { return axios.get(`${window.BASEDOMAIN}/app/link/getShorUrl?url=${params}`).then(res => res.data) }
+export const getMobileUrl= params => { return axios.post(`${base}/getMobileUrl`, params).then(res => res.data) }
+
+//分页获取刮刮乐活动列表/
+export const getActList = params => { return axios.post(`${base}/getScratchList`, params).then(res => res.data) }
