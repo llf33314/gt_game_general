62dda08d815c3d7d497390421d118f5504d6c517
ZHONGHUIDI <1012560375@qq.com>
2018-01-24 16:03:18 +0800

    游戏

--- cli-pc/src/components/openBox/vue/addAct.vue
+++ cli-pc/src/components/openBox/vue/addAct.vue
@@ -239,11 +239,11 @@
         <div class="h80"></div> 
         <div class="btnRow"  v-if="this.active!=5">
             <el-button   @click="upStep()" v-if="this.active!=0">上一步</el-button>
-            <el-button type="primary" @click="next1('ruleForm1')" v-if="this.active==0">下一步1</el-button> 
-            <el-button type="primary" @click="next('ruleForm2')" v-if="this.active==1">下一步2</el-button>
-            <el-button type="primary" @click="next('ruleForm3')" v-if="this.active==2">下一步3</el-button>   
+            <el-button type="primary" @click="next1('ruleForm1')" v-if="this.active==0">下一步</el-button> 
+            <el-button type="primary" @click="next('ruleForm2')" v-if="this.active==1">下一步</el-button>
+            <el-button type="primary" @click="next('ruleForm3')" v-if="this.active==2">下一步</el-button>   
             <el-button type="primary" @click="lastStep()"        v-if="this.active==3">保存</el-button>   
-            <el-button type="primary" @click="submit()">打印</el-button>   
+            <!-- <el-button type="primary" @click="submit()">打印</el-button>    -->
         </div> 
     </div>   
 </div>
