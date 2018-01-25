df5ff47c1c116b1435c5b78b4d602d4c72fc67d9
yanhuiting <1227406194@qq.com>
2018-01-25 16:03:19 +0800

    ...

--- cli-pc/src/components/bigTurnplate/api/api.js
+++ cli-pc/src/components/bigTurnplate/api/api.js
@@ -1,4 +1,42 @@
-import axios from './../../../http' 
-let base = window.BASEDOMAIN
-/*短连接*/
-export const requestgetShortUrl = params => { return axios.get(`${base}/app/link/getShorUrl?url=${params}`).then(res => res.data) }
+import axios from './../../../http'
+let base = window.BASEDOMAIN + '/app/turntable'
+
+//新增大转盘活动/
+export const saveAct = params => {
+  return axios.post(`${base}/addScratch`, params).then(res => res.data)
+}
+
+//统计大转盘活动总数/
+export const getActCount = params => {
+  return axios.post(`${base}/countTurntable`, params).then(res => res.data)
+}
+
+//通过活动id查询大转盘活动/
+export const getActivityById = params => {
+  return axios.post(`${base}/getActivityById`, params).then(res => res.data)
+}
+
+/*获取手机端链接*/
+export const getShortUrl = params => {
+  return axios.get(`${window.BASEDOMAIN}/app/link/getShorUrl?url=${params}`).then(res => res.data)
+}
+
+export const getMobileUrl = params => {
+  return axios.post(`${base}/getMobileUrl`, params).then(res => res.data)
+}
+
+//分页获取大转盘活动列表/
+export const getActList = params => {
+  return axios.post(`${base}/getTurntableList`, params).then(res => res.data)
+}
+
+//编辑大转盘活动设置/
+export const modfiyScratch = params => {
+  return axios.post(`${base}/modfiyTurntable`, params).then(res => res.data)
+}
+
+
+//大转盘活动暂停/开始活动
+export const stopLuck = params => {
+  return axios.post(`${base}/stopTurntable`, params).then(res => res.data)
+}
