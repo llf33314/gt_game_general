8722cadc062d93ead677765beee7e61de3ea0f29
ZHONGHUIDI <1012560375@qq.com>
2018-01-31 08:41:11 +0800

    升级2.0

--- cli-pc/src/main.js
+++ cli-pc/src/main.js
@@ -13,7 +13,8 @@ Object.keys(filters).forEach(key => {
 });
 //加载全局组件
 import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-default/index.css'
+// import 'element-ui/lib/theme-default/index.css'
+import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(ElementUI)
 const GtNullData = ()=>import('./components/PublicVue/null-data/nullData')
 const GtCopyUrl = ()=>import('./components/PublicVue/copy-url/copy-url')
